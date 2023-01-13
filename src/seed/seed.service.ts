ef582d0255d3efcdf8f216b0d69f90343fb3712d
Ludwing Rivera Amador <ludwing.ra@gmail.com>
2023-01-12 22:15:19 -0500

    feat: :sparkles: Inser Promise.all

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany();
+
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    const insertPromisesArray = [];
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
     });
 
-    return data.results;
+    await Promise.all(insertPromisesArray);
+
+    return 'Seed executed successfully ';
   }
 }
